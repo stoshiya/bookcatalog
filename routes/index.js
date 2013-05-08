210e824dfbdce433d074aea503ea9a4fae0a4e55
Toshiya SAITOH <toshiya@saitoh.nu>
2013-05-09 08:18:36 +0900

    Modified to display scraping result.

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,28 @@
+var async = require('async')
+  , scraper = require('./../lib/scrape');
+
 exports.index = function(req, res){
-  res.render('index', { title: 'Book Catalog', profile: req.session.passport.user });
+  async.parallel({
+    oreilly: function(callback) {
+      scraper.oreilly(function(array) {
+        callback(null, array);
+      });
+    },
+    computerbookjp: function(callback) {
+      scraper.computerbookjp(function(array) {
+        callback(null, array);
+      });
+    }
+  }, function(err, results) {
+    if (err) {
+      res.send(500, err);
+      return;
+    }
+    res.render('index', {
+      title: 'Book Catalog',
+      profile: req.session.passport.user,
+      oreilly: results.oreilly,
+      computerbookjp: results.computerbookjp
+    })
+  });
 };
