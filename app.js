a55cd5a850ed0e6a6229767324df3a52080a0ad6
Toshiya SAITOH <toshiya@saitoh.nu>
2013-06-04 00:29:22 +0900

    Removed twitter OAuth.

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var express = require('express')
   , http = require('http')
   , path = require('path')
   , passport = require('passport')
-  , TwitterStrategy = require('passport-twitter').Strategy
   , GithubStrategy = require('passport-github').Strategy;
 
 passport.serializeUser(function(user, done) {
@@ -14,37 +13,13 @@ passport.deserializeUser(function(obj, done) {
   done(null, obj);
 });
 
-var twitterKey     = process.env.TWITTER_CONSUMER_KEY;
-var twitterSecret  = process.env.TWITTER_CONSUMER_SECRET;
 var githubClientId = process.env.GITHUB_CLIENT_ID;
 var githubSecret   = process.env.GITHUB_CLIENT_SECRET;
-
-if (!twitterKey || !twitterSecret) {
-  console.error('require key and secret for twitter.');
-  process.exit(1);
-}
 
 if (!githubClientId || !githubSecret) {
   console.error('require id and secret for github.');
   process.exit(1);
 }
-
-passport.use(new TwitterStrategy({
-    consumerKey:    twitterKey,
-    consumerSecret: twitterSecret
-  },
-  function(token, tokenSecret, profile, done) {
-    passport.session.accessToken = token;
-    passport.session.profile = profile;
-    process.nextTick(function() {
-      done(null, {
-        id:       profile.id,
-        username: profile.username,
-        photo:    profile.photos[0].value
-      });
-    });
-  }
-));
 
 passport.use(new GithubStrategy({
     clientID:     githubClientId,
@@ -53,7 +28,6 @@ passport.use(new GithubStrategy({
   function(token, tokenSecret, profile, done) {
     passport.session.accessToken = token;
     passport.session.profile = profile;
-    console.log(profile);
     process.nextTick(function() {
       done(null, {
         id:       profile.id,
@@ -86,32 +60,17 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-app.get('/:index?', ensureGithubAuthenticated, routes.index);
-//app.get('/login', routes.login);
+app.get('/', routes.index);
 
-app.get('/auth/twitter',   passport.authenticate('twitter'));
-app.get('/auth/twitter/callback', passport.authenticate('twitter', {
+app.get('/login',    passport.authenticate('github'));
+app.get('/callback', passport.authenticate('github', {
   successRedirect: '/',
-  failureRedirect: '/login' // TODO
+  failureRedirect: '/login'
 }));
 
-app.get('/auth/github',          passport.authenticate('github'));
-app.get('/auth/github/callback', passport.authenticate('github', {
-  successRedirect: '/',
-  failureRedirect: '/login' // TODO
-}));
-
-function ensureTwitterAuthenticated(req, res, next) {
+function ensureAuthenticated(req, res, next) {
   if (!req.isAuthenticated()) {
-    res.redirect('/auth/twitter');
-    return;
-  }
-  next();
-}
-
-function ensureGithubAuthenticated(req, res, next) {
-  if (!req.isAuthenticated()) {
-    res.redirect('/auth/github');
+    res.redirect('/login');
     return;
   }
   next();
