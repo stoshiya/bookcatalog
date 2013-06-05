b1de18e3e28e1fb124effb104c1f5ecc7a0bb56f
Toshiya SAITOH <toshiya@saitoh.nu>
2013-06-06 01:41:29 +0900

    Corrected response for unauthorized client, and so on.

--- routes/index.js
+++ routes/index.js
@@ -30,19 +30,20 @@ exports.index = function(req, res){
     res.render('index', {
       title: 'Book Catalog',
       profile: req.session.passport.user,
-      amazon: results.amazon,
-      oreilly: results.oreilly,
+      amazon:         results.amazon,
+      oreilly:        results.oreilly,
       computerbookjp: results.computerbookjp
     });
   });
 };
 
 exports.checkout = function(req, res) {
-  var isMember = req.session.passport.user.member;
-  if (!isMember) {
-    res.send(550, 'permittion denied');
+  var passport = req.session.passport;
+  if (typeof passport === 'undefined' || typeof passport.user === 'undefined' || !passport.user.member) {
+    res.send(401);
     return;
   }
+
   var books = [];
   for (var key in req.body) {
     if (req.body.hasOwnProperty(key)) {
@@ -50,12 +51,14 @@ exports.checkout = function(req, res) {
     }
   }
   async.each(books, function(book, callback) {
-    Book.findOneAndUpdate({ isbn: book.isbn }, book, { upsert:true }, function(err) {
-      if (err) callback(err);
-      else     callback();
+    Book.findOneAndUpdate({ isbn: book.isbn }, book, { upsert: true }, function(err) {
+      callback(err);
     });
   }, function(err) {
-    if (err) res.send(500, err);
-    else     res.send(201, 'Success to checkout');
+    if (err) {
+      res.send(500, err);
+      return;
+    }
+    res.send(201, 'Success to checkout');
   });
 };
