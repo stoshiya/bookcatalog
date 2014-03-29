f2004b20d60ee093ac48b3553b37ef7ac1df362d
Toshiya SAITOH <stoshiya@gmail.com>
2014-03-29 12:34:28 +0900

    Added favicon and updated connect 3.0.

--- app.js
+++ app.js
@@ -69,9 +69,10 @@ app.configure(function() {
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
-  app.use(express.favicon());
+  app.use(express.favicon(path.join(__dirname, 'public', 'favicon.ico')));
   app.use(express.logger('dev'));
-  app.use(express.bodyParser());
+  app.use(express.json());
+  app.use(express.urlencoded());
   app.use(express.methodOverride());
   app.use(express.cookieParser());
   app.use(express.session({ secret: 'secret' }));
